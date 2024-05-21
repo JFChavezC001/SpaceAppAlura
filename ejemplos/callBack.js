b60ee066f05ec2d904519a65619aac455126f27d
JFChavezC001 <jfchavezc001@gmail.com>
2024-05-21 14:12:09 -0600

    Agregamos dentro del Script de callback unos ejemplos de como funciona, ademas de ejemplos relacionados a funciones anonimas, arrow functions y callbackHell

--- ejemplos/callBack.js
+++ ejemplos/callBack.js
@@ -1,10 +1,44 @@
-function uno () {
+function uno (dos) {
     console.log("uno")
+    setTimeout(dos, 2000);
 }
-function dos () {
+function dos (tres) {
     console.log("dos")
+    setTimeout(tres, 2000);
 }
 
+function tres (cuatro) {
+    console.log("tres")
+    setTimeout(cuatro, 2000);
+}
+
+function cuatro (cinco) {
+    console.log("cuatro")
+    setTimeout(cinco, 2000);
+}
+
+function cinco () {
+    console.log("cinco")
+}
+
+setTimeout( ()=> uno(dos),2000)
+
+/*
 //funcion aasincrona
-setTimeout(uno,5000)//recibe otra funcion como parametro
-dos()
+setTimeout(()=> uno(dos),5000)//recibe otra funcion como parametro
+
+
+//funcion anonima
+
+const nombre = function(){
+    console.log("nombre");
+}
+nombre()
+
+//arrow functions
+const persona = () => {console.log("Barbara");}//no es necesario poner las llaves cuando solo es una linea de codigo
+
+persona();
+
+()=> console.log();
+*/
